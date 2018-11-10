15bd66b9b2e620ea367420b2920ce2279537e9e2
Thomas Elliott <tom@MacBook-Pro.localdomain>
2018-11-10 01:10:05 -0500

    response

--- static/contact.js
+++ static/contact.js
@@ -14,7 +14,9 @@ function contactFormHandler(e) {
   const formData = new URLSearchParams(new FormData(contactForm));
   fetch('/contact', { method: 'POST', body: formData })
     .then(r => {
-      console.log(JSON.stringify(r));
+      r.json().then(d => {
+        console.log(d);
+      });
     })
     .catch(e => {
       console.log(e);
